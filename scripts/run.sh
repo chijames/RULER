c3c014d69c0f77b7111d8460af779036aab178c4
Ying Sheng <sqy1415@gmail.com>
2024-08-01 00:31:48 +0000

    add cache for prepare

--- scripts/run.sh
+++ scripts/run.sh
@@ -24,7 +24,6 @@ fi
 
 # Root Directories
 GPUS="1" # GPU size for tensor_parallel.
-ROOT_DIR="benchmark_root" # the path that stores generated task samples and model predictions.
 MODEL_DIR="../.." # the path that contains individual model folders from HUggingface.
 ENGINE_DIR="." # the path that contains individual engine folders from TensorRT-LLM.
 BATCH_SIZE=1000  # increase to improve GPU utilization
@@ -57,6 +56,7 @@ if [ -z "${TASKS}" ]; then
     exit 1
 fi
 
+ROOT_DIR="benchmark_root/${NUM_SAMPLES}samples" # the path that stores generated task samples and model predictions.
 SEQ_LENGTHS=(
     # 131072
     128000
